6af66fec330e8e71328f0d886e86fe6f2970033e
Gorr <gorrgorrob@gmail.com>
2020-04-02 12:44:57 +0500

    Architecture update & complete command

--- src/main/kotlin/ru/catstack/todo/ui/TodoListScreen.kt
+++ src/main/kotlin/ru/catstack/todo/ui/TodoListScreen.kt
@@ -4,6 +4,7 @@ import org.kodein.di.generic.instance
 import ru.catstack.todo.di.kodein
 import ru.catstack.todo.engine.BaseScreen
 import ru.catstack.todo.model.Command
+import ru.catstack.todo.ui.TodoListViewModel.ResponseState
 import java.lang.StringBuilder
 
 class TodoListScreen : BaseScreen() {
@@ -15,6 +16,7 @@ class TodoListScreen : BaseScreen() {
         Command("help", "shows the help text", ::executeHelp),
         Command("add", "add new task. Usage: 'add message'", ::executeAdd),
         Command("delete", "deletes task by number. Usage: 'delete 1'", ::executeDelete),
+        Command("complete", "switch complete value to selected task. Usage: 'complete 1'", ::executeComplete),
         Command("print", "prints all tasks", ::printList),
         Command("exit", "close the application", ::executeExit)
     ).associateBy { it.name }
@@ -69,37 +71,38 @@ class TodoListScreen : BaseScreen() {
     }
 
     private fun executeAdd(args: List<String>) {
-        if (args.isNotEmpty()) {
-            val textBuilder = StringBuilder()
-            args.forEach { textBuilder.append("$it ") }
-            textBuilder.deleteCharAt(textBuilder.lastIndex)
-            val text = textBuilder.toString()
-
-            viewModel.addTask(text)
-
-            println("Successful")
-        } else {
-            println("You must add message to task. Example: 'add message'")
+        when (viewModel.addTask(args)) {
+            ResponseState.Successful ->
+                println("Successful")
+            ResponseState.MissingArguments ->
+                println("You must add message to task. Example: 'add message'")
+            else -> println("Unexpected error")
         }
     }
 
     private fun executeDelete(args: List<String>) {
-        if (args.isEmpty()) {
-            println("You must add task number. Example: 'delete 1'")
-            return
+        when (val response = viewModel.deleteTask(args)) {
+            ResponseState.MissingArguments ->
+                println("You must add task number. Example: 'delete 1'")
+            is ResponseState.IncorrectNumber ->
+                println("Incorrect number")
+            ResponseState.Successful ->
+                println("Successful")
+            is ResponseState.TaskIsNotExists ->
+                println("Task with number ${response.taskIndex + 1} doesn't exists")
         }
+    }
 
-        val taskNumber = args.first().toIntOrNull()
-        if (taskNumber == null) {
-            println("Incorrect number")
-            return
-        }
-
-        if (viewModel.todoList.size >= taskNumber && taskNumber >= 1) {
-            viewModel.deleteTask(taskNumber - 1)
-            println("Successful")
-        } else {
-            println("Task with number $taskNumber doesn't exists")
+    private fun executeComplete(args: List<String>) {
+        when (val response = viewModel.completeTask(args)) {
+            ResponseState.MissingArguments ->
+                println("You must add task number. Example: 'complete 1'")
+            is ResponseState.IncorrectNumber ->
+                println("Incorrect number")
+            ResponseState.Successful ->
+                println("Successful")
+            is ResponseState.TaskIsNotExists ->
+                println("Task with number ${response.taskIndex + 1} doesn't exists")
         }
     }
 
--- src/main/kotlin/ru/catstack/todo/ui/TodoListViewModel.kt
+++ src/main/kotlin/ru/catstack/todo/ui/TodoListViewModel.kt
@@ -2,24 +2,81 @@ package ru.catstack.todo.ui
 
 import ru.catstack.todo.data.TodoRepository
 import ru.catstack.todo.model.Task
+import java.lang.StringBuilder
 
 class TodoListViewModel(private val repository: TodoRepository) {
+    sealed class ResponseState {
+        object Successful : ResponseState()
+        object MissingArguments : ResponseState()
+        data class IncorrectNumber(val input: String) : ResponseState()
+        data class TaskIsNotExists(val taskIndex: Int) : ResponseState()
+    }
+
     private var mutableTodoList = ArrayList<Task>()
     val todoList: List<Task>
-            get() = mutableTodoList
+        get() = mutableTodoList
 
     fun loadTodoList() {
         if (todoList.isEmpty())
             mutableTodoList = repository.loadTodoList()
     }
 
-    fun addTask(todoText: String) {
+    fun addTask(args: List<String>): ResponseState {
+        if (args.isNotEmpty()) {
+            val textBuilder = StringBuilder()
+            args.forEach { textBuilder.append("$it ") }
+            textBuilder.deleteCharAt(textBuilder.lastIndex)
+            val text = textBuilder.toString()
+
+            addTask(text)
+
+            return ResponseState.Successful
+        } else {
+            return ResponseState.MissingArguments
+        }
+    }
+
+    private fun addTask(todoText: String) {
         mutableTodoList.add(Task(todoText, false))
         saveToRepository()
     }
 
-    fun deleteTask(taskIndex: Int) {
+    fun deleteTask(args: List<String>): ResponseState {
+        if (args.isEmpty())
+            return ResponseState.MissingArguments
+
+        val taskNumber = args.first().toIntOrNull() ?: return ResponseState.IncorrectNumber(args.first())
+
+        if (todoList.size >= taskNumber && taskNumber >= 1) {
+            deleteTask(taskNumber - 1)
+            return ResponseState.Successful
+        } else {
+            return ResponseState.TaskIsNotExists(taskNumber - 1)
+        }
+    }
+
+    private fun deleteTask(taskIndex: Int) {
         mutableTodoList.removeAt(taskIndex)
+        saveToRepository()
+    }
+
+    fun completeTask(args: List<String>): ResponseState {
+        if (args.isEmpty())
+            return ResponseState.MissingArguments
+
+        val taskNumber = args.first().toIntOrNull() ?: return ResponseState.IncorrectNumber(args.first())
+
+        if (todoList.size >= taskNumber && taskNumber >= 1) {
+            completeTask(taskNumber - 1)
+            return ResponseState.Successful
+        } else {
+            return ResponseState.TaskIsNotExists(taskNumber - 1)
+        }
+    }
+
+    private fun completeTask(taskIndex: Int) {
+        val task = mutableTodoList[taskIndex]
+        task.isCompleted = !task.isCompleted
         saveToRepository()
     }
 
